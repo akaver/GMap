26426e177cb91170d3f6b5df19cb8150e43b1eca
akaver <git@akaver.com>
2016-05-02 17:21:40 +0300

    updates

--- app/src/main/java/com/akaver/gmap/MainActivity.java
+++ app/src/main/java/com/akaver/gmap/MainActivity.java
@@ -3,17 +3,21 @@ package com.akaver.gmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    private final static String TAG = "MainActivity";
 
     private GoogleMap mGoogleMap;
 
+    private Menu mOptionsMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,32 +26,61 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.gmap);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        mOptionsMenu = menu;
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()){
+            case R.id.menu_maptype_hybrid:
+            case R.id.menu_maptype_none:
+            case R.id.menu_maptype_normal:
+            case R.id.menu_maptype_satellite:
+            case R.id.menu_maptype_terrain:
 
-        //noinspection SimplifiableIfStatementx
-        if (id == R.id.action_settings) {
-            return true;
+                item.setChecked(true);
+                changeMapType();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void changeMapType(){
+        if (mGoogleMap == null){
+            return;
         }
 
-        return super.onOptionsItemSelected(item);
+        if (mOptionsMenu.findItem(R.id.menu_maptype_hybrid).isChecked()){
+            mGoogleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        } else if (mOptionsMenu.findItem(R.id.menu_maptype_none).isChecked()){
+            mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+        } else if (mOptionsMenu.findItem(R.id.menu_maptype_normal).isChecked()){
+            mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        } else if (mOptionsMenu.findItem(R.id.menu_maptype_satellite).isChecked()){
+            mGoogleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        } else if (mOptionsMenu.findItem(R.id.menu_maptype_terrain).isChecked()){
+            mGoogleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        }
+
+        Log.d(TAG,"Map type changed!");
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mGoogleMap = googleMap;
+        Log.d(TAG,"Google MAP ready!");
     }
 }
